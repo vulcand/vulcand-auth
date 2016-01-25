c23c8ac02fb10e7322d8a178deaec05ba0a01093
Anton Galitsyn <anton.galitsyn@gmail.com>
2016-01-25 13:53:36 +0600

    Fix vulcan imports to github.com/vulcand (not mailgun)

--- auth/auth.go
+++ auth/auth.go
@@ -7,9 +7,9 @@ import (
 	"io"
 	"net/http"
 
-	"github.com/mailgun/vulcand/Godeps/_workspace/src/github.com/codegangsta/cli"
-	"github.com/mailgun/vulcand/Godeps/_workspace/src/github.com/mailgun/oxy/utils"
-	"github.com/mailgun/vulcand/plugin"
+	"github.com/vulcand/vulcand/Godeps/_workspace/src/github.com/codegangsta/cli"
+	"github.com/vulcand/vulcand/Godeps/_workspace/src/github.com/vulcand/oxy/utils"
+	"github.com/vulcand/vulcand/plugin"
 )
 
 const Type = "auth"
